2a039bd2c70a9ef3881b2e5233b7831a183a2074
Mohamed Alsharaf <mohamed.alsharaf@gmail.com>
2013-09-24 22:21:02 +1200

    update FlashCardAdminBundle & FlashCardBundle to v1.0.0

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,15 +17,33 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Moo\FlashCardBundle\MooFlashCardBundle(),
+
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\jQueryBundle\SonatajQueryBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\DoctrineORMAdminBundle\SonataDoctrineORMAdminBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\UserBundle\SonataUserBundle('FOSUserBundle'),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Application\Sonata\UserBundle\ApplicationSonataUserBundle(),
+
+            new Moo\FlashCardAdminBundle\MooFlashCardAdminBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+
+            $bundles[] =new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle();
+            $bundles[] =new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+            $bundles[] =new Liip\FunctionalTestBundle\LiipFunctionalTestBundle();
         }
 
         return $bundles;
